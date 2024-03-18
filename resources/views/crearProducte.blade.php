217f60dc9db3924cfa3207469b661bf39db3edf8
Zenon <zenropru@gmail.com>
2024-03-18 17:56:37 +0100

    crear

--- resources/views/crearProducte.blade.php
+++ resources/views/crearProducte.blade.php
@@ -1,5 +1,80 @@
 @extends('master')
+<style>
+        body {
+            font-family: "Lato", sans-serif;
+            background-color: #115571;
+            overflow-x: hidden; /* Evita el desplazamiento horizontal */
+            margin: 0;
+            padding: 0;
+        }
 
+        /* Estilo para el sidebar */
+        .sidenav {
+            height: 100%;
+            width: 250px; /* Ancho del sidebar */
+            position: fixed;
+            z-index: 1;
+            top: 0;
+            left: 0;
+            background-color: #fff; /* Fondo semi-transparente */
+            overflow-x: hidden;
+            transition: 0.5s;
+            padding-top: 60px;
+        }
+
+        /* Estilo para los enlaces del sidebar */
+        .sidenav a {
+            padding: 8px 8px 8px 32px;
+            text-decoration: none;
+            font-size: 25px;
+            color: #000000; /* Color de texto blanco */
+            display: block;
+            transition: 0.3s;
+        }
+
+        .sidenav a:hover {
+            color: #87CEFA;
+        }
+
+        /* Estilo para el contenido principal */
+        .content {
+            margin-left: 250px; /* Ancho del sidebar */
+            padding: 16px;
+            background-color: #115571;
+        }
+
+        /* Estilo para el botón de abrir */
+        .openbtn {
+            font-size: 30px;
+            cursor: pointer;
+        }
+
+        /* Estilo para el formulario */
+        .form-container {
+            max-width: 100%;
+            margin: auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin-top: 50px;
+        }
+
+        /* Estilo para el botón del formulario */
+        .btn-primary {
+            background-color: #4CAF50;
+            border: none;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+            border-radius: 5px;
+        }
+    </style>
 @section('body')
 
     <div class="navbar">
