db0cd3a3355c45b2f46ecc46540fe3b57e57aede
paugb29 <126184749+paugb29@users.noreply.github.com>
2024-04-09 00:44:05 +0200

    traduccio productes + categoria

--- resources/views/crearCategoria.blade.php
+++ resources/views/crearCategoria.blade.php
@@ -6,20 +6,20 @@
         <div class="col-md-6">
             <div class="card">
                 <div class="card-header">
-                    <h1 class="card-title">Crear Nova Categoría</h1>
+                    <h1 class="card-title">{{ __('productes.crearCategoria') }}</h1>
                 </div>
                 <div class="card-body">
                     <form action="{{ route('categorias.store') }}" method="POST">
                         @csrf
                         <div class="mb-3">
-                            <label for="Categoria" class="form-label">Nom de la categoría:</label>
+                            <label for="Categoria" class="form-label">{{ __('productes.nom') }}</label>
                             <input type="text" name="Categoria" id="Categoria" class="form-control" required>
                         </div>
                         <!-- Agrega más campos según sea necesario -->
 
                         <div class="d-grid gap-2">
-                            <button type="submit" class="btn btn-primary">Crear Categoría</button>
-                            <a href="{{ route('mostrarProductos') }}" class="btn btn-secondary mt-3">Volver</a>
+                            <button type="submit" class="btn btn-primary">{{ __('productes.crearCategoria') }}</button>
+                            <a href="{{ route('mostrarProductos') }}" class="btn btn-secondary mt-3">{{ __('productes.tornar') }}</a>
                         </div>
                     </form>
                 </div>
