798b86b16b5a8bae69f0ab4e131ffc23fb897dba
paugb29 <pgordillobruguera@gmail.com>
2024-04-16 20:51:06 +0200

    acces rutas restringit per rol arreglat per noves rutas

--- app/Http/Middleware/RoleCheckMiddleware.php
+++ app/Http/Middleware/RoleCheckMiddleware.php
@@ -18,10 +18,10 @@ class RoleCheckMiddleware
         }
 
         // Si el usuario no es admin, pero es usuari, restringir acceso a rutas de crear
-        if ($user->role === 'usuari' && ($request->is('productes/create') || $request->is('crearCategorias') || $request->is('clientes/create')|| $request->is('personal'))) {
+        if ($user->role === 'usuari' && ($request->is('productes/create') || $request->routeIs('productes.show') || $request->is('crearCategorias') || $request->is('clientes/create')|| $request->is('personal')|| $request->is('ventas/create')|| $request->is('compras/create')|| $request->is('venedors/create'))) {
             return redirect()->route('error')->with('error', 'No tienes permiso para acceder a esta página');
         }
-        if ($user->role === 'venta' && ($request->is('productes')  || $request->is('personal') || $request->is('clientes'))){
+        if ($user->role === 'venta' && ($request->is('producte')  ||  $request->is('productes')  || $request->is('products')  || $request->is('personal') || $request->is('clientes')|| $request->is('venedors')|| $request->is('facturas')|| $request->is('compras'))){
             return redirect()->route('errorVenta')->with('error', 'No tienes permiso para acceder a esta página');
         }
                 
--- routes/web.php
+++ routes/web.php
@@ -32,103 +32,93 @@ Route::middleware(['auth'])->group(function () {
     // Rutas accesibles para todos los usuarios autenticados
 
 
-    Route::get('/index', function () {
-        return view('index');
-    })->middleware(['verified'])->name('index');
-
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/personal', [ProfileController::class, 'showAllUsers'])->name('personal');
-
-    Route::get('/index', [IndexController::class, 'showLastUsersAndGraph'])->name('indexPrincipal');
-    Route::get('/', [IndexController::class, 'showLastUsersAndGraph'])->name('indexPrincipal');
-
-    Route::get('/crear-categoria', [ProducteController::class, 'mostrarFormularioCrearCategoria'])->name('crear-categoria');
-    Route::post('/crear-categoria', [ProducteController::class, 'crearCategoria'])->name('productos.crear-categoria');
-
-    Route::get('/crearCategorias', [CategoriaController::class, 'create'])->name('categorias.create');
-    Route::post('/crearCategorias', [CategoriaController::class, 'store'])->name('categorias.store');
-    Route::get('/mostrarCategorias/{categoria}', [CategoriaController::class, 'show'])->name('categorias.show');
-    Route::get('/categorias/{categoria}/edit', [CategoriaController::class, 'edit'])->name('categorias.edit');
-    Route::delete('/categorias/{categoria}', [CategoriaController::class, 'destroy'])->name('categorias.destroy');
-    Route::put('/categorias/{categoria}', [CategoriaController::class, 'update'])->name('categorias.update');
-
-    Route::resource('clientes', ClienteController::class);
-    Route::get('/mostrarClientes', [ClienteController::class, 'mostrarTodos'])->name('mostrarClientes');
-
-    Route::post('/cambiar-idioma/{idioma}', [LanguageController::class, 'cambiarIdioma'])->name('cambiar-idioma');
-
-
-Route::get('/orders/{id}', [VentaPropuestaController::class, 'generateInvoice'])->name('orders.generateInvoice');
-Route::get('/orders/{id}/generate-invoice-buy', [CompraController::class, 'generateInvoiceBuy'])->name('orders.generateInvoiceBuy');
-
-Route::get('/facturas', [CompraProductoController::class, 'index'])->name('invoices.index');
-
-Route::get('/orders/{id}/all-invoice-sell', [CompraProductoController::class, 'generateInvoiceAllSell'])->name('orders.generateInvoiceAllSell');
-Route::get('/orders/{id}/all-invoice', [CompraProductoController::class, 'generateInvoiceAllBuy'])->name('orders.generateInvoiceAllBuy');
-    Route::get('/graficPoblacio', [ClienteController::class, 'graficPoblacio'])->name('graficPoblacio');
-    Route::get('/graficEstat', [VentaPropuestaController::class, 'graficEstat'])->name('ventas.graficEstat');
-    Route::get('/error', function () {
-        return view('error');
-    })->name('error');
-
-    Route::get('/crearProducte', function () {
-        return view('crearProducte');
-    });
-    Route::get('/productes', [ProducteController::class, 'index']);
-
-    Route::get('/mostrarProductes', function () {
-        $productos = Producte::all(); // Suponiendo que Producto es tu modelo para productos
-        return view('mostrarProductes', compact('productos'));
-    })->name('mostrarProductes');
-    Route::get('/productes/create', [ProducteController::class, 'create'])->name('productes.create');
-    Route::post('/productes', [ProducteController::class, 'store'])->name('productes.store');
-    Route::resource("producte", ProducteController::class);
-    Route::get('/productes', [ProducteController::class, 'index'])->name('producte.index');
-
-    Route::get('/products', [ProducteController::class, 'productos'])->name('products');
-    Route::get('/mostrarProductos', [ProducteController::class, 'mostrarProductos'])->name('mostrarProductos');
-
-
-    Route::get('/productes/{producte}', [ProducteController::class,'show'])->name('productes.show');
-    Route::put('/productes/{producte}', [ProducteController::class, 'update'])->name('productes.update');
-    Route::delete('/productes/{producte}', [ProducteController::class, 'destroy'])->name('productes.destroy');
-
-    Route::resource('ventas', VentaPropuestaController::class);
-    Route::post('/ventas/{id}/cambiarEstado', [VentaPropuestaController::class, 'cambiarEstado'])->name('ventas.cambiarEstado');
-    Route::post('ventas/storeProductes',  [VentaPropuestaController::class, 'storeProductes'])->name('ventas.storeProductes');
-    Route::get('ventas/posarProductesProposta/{id}', [VentaPropuestaController::class, 'posarProductesProposta'])->name('ventas.posarProductesProposta');
-    Route::get('/venta-propuesta/{id}', [VentaPropuestaController::class, 'show'])->name('VentaPropuesta.show');
-    Route::get('/mostrarventas/{id}', [VentaPropuestaController::class, 'index_ventas'])->name('mostrarventas');
-
-    Route::get('/errorVenta', function () {
-        return view('errorVenta');
-    })->name('errorVenta');
-
+ 
     // Lógica para restringir el acceso según el rol del usuario
     Route::middleware(['role.check'])->group(function () {
-        Route::get('/productes/create', [ProducteController::class, 'create'])->name('productes.create');
-        Route::post('/productes', [ProducteController::class, 'store'])->name('productes.store');
+        Route::get('/index', function () {
+            return view('index');
+        })->middleware(['verified'])->name('index');
+    
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+        Route::get('/personal', [ProfileController::class, 'showAllUsers'])->name('personal');
+    
+        Route::get('/index', [IndexController::class, 'showLastUsersAndGraph'])->name('indexPrincipal');
+        Route::get('/', [IndexController::class, 'showLastUsersAndGraph'])->name('indexPrincipal');
+    
+        Route::get('/crear-categoria', [ProducteController::class, 'mostrarFormularioCrearCategoria'])->name('crear-categoria');
+        Route::post('/crear-categoria', [ProducteController::class, 'crearCategoria'])->name('productos.crear-categoria');
+    
         Route::get('/crearCategorias', [CategoriaController::class, 'create'])->name('categorias.create');
         Route::post('/crearCategorias', [CategoriaController::class, 'store'])->name('categorias.store');
-        Route::get('/personal', [ProfileController::class, 'showAllUsers'])->name('personal');
-         Route::resource('clientes', ClienteController::class)->except(['create']);
-         Route::get('/clientes/create', [ClienteController::class, 'create'])->name('clientes.create');
-         Route::resource('ventas', VentaPropuestaController::class);
-         Route::get('/venta-propuesta/{id}', [VentaPropuestaController::class, 'show'])->name('VentaPropuesta.show');
-         Route::get('/productes', [ProducteController::class, 'index'])->name('producte.index');
-
-
+        Route::get('/mostrarCategorias/{categoria}', [CategoriaController::class, 'show'])->name('categorias.show');
+        Route::get('/categorias/{categoria}/edit', [CategoriaController::class, 'edit'])->name('categorias.edit');
+        Route::delete('/categorias/{categoria}', [CategoriaController::class, 'destroy'])->name('categorias.destroy');
+        Route::put('/categorias/{categoria}', [CategoriaController::class, 'update'])->name('categorias.update');
+    
+        Route::resource('clientes', ClienteController::class);
+        Route::get('/mostrarClientes', [ClienteController::class, 'mostrarTodos'])->name('mostrarClientes');
+    
+        Route::post('/cambiar-idioma/{idioma}', [LanguageController::class, 'cambiarIdioma'])->name('cambiar-idioma');
+    
+    
+    Route::get('/orders/{id}', [VentaPropuestaController::class, 'generateInvoice'])->name('orders.generateInvoice');
+    Route::get('/orders/{id}/generate-invoice-buy', [CompraController::class, 'generateInvoiceBuy'])->name('orders.generateInvoiceBuy');
+    
+    Route::get('/facturas', [CompraProductoController::class, 'index'])->name('invoices.index');
+    
+    Route::get('/orders/{id}/all-invoice-sell', [CompraProductoController::class, 'generateInvoiceAllSell'])->name('orders.generateInvoiceAllSell');
+    Route::get('/orders/{id}/all-invoice', [CompraProductoController::class, 'generateInvoiceAllBuy'])->name('orders.generateInvoiceAllBuy');
+        Route::get('/graficPoblacio', [ClienteController::class, 'graficPoblacio'])->name('graficPoblacio');
+        Route::get('/graficEstat', [VentaPropuestaController::class, 'graficEstat'])->name('ventas.graficEstat');
+        Route::get('/error', function () {
+            return view('error');
+        })->name('error');
+    
+        Route::get('/crearProducte', function () {
+            return view('crearProducte');
+        });
+        Route::get('/productes', [ProducteController::class, 'index']);
+    
+        Route::get('/mostrarProductes', function () {
+            $productos = Producte::all(); // Suponiendo que Producto es tu modelo para productos
+            return view('mostrarProductes', compact('productos'));
+        })->name('mostrarProductes');
+        Route::get('/productes/create', [ProducteController::class, 'create'])->name('productes.create');
+        Route::post('/productes', [ProducteController::class, 'store'])->name('productes.store');
+        Route::resource("producte", ProducteController::class);
+        Route::get('/productes', [ProducteController::class, 'index'])->name('producte.index');
+    
+        Route::get('/products', [ProducteController::class, 'productos'])->name('products');
+        Route::get('/mostrarProductos', [ProducteController::class, 'mostrarProductos'])->name('mostrarProductos');
+    
+    
+        Route::get('/productes/{producte}', [ProducteController::class,'show'])->name('productes.show');
+        Route::put('/productes/{producte}', [ProducteController::class, 'update'])->name('productes.update');
+        Route::delete('/productes/{producte}', [ProducteController::class, 'destroy'])->name('productes.destroy');
+    
+        Route::resource('ventas', VentaPropuestaController::class);
+        Route::post('/ventas/{id}/cambiarEstado', [VentaPropuestaController::class, 'cambiarEstado'])->name('ventas.cambiarEstado');
+        Route::post('ventas/storeProductes',  [VentaPropuestaController::class, 'storeProductes'])->name('ventas.storeProductes');
+        Route::get('ventas/posarProductesProposta/{id}', [VentaPropuestaController::class, 'posarProductesProposta'])->name('ventas.posarProductesProposta');
+        Route::get('/venta-propuesta/{id}', [VentaPropuestaController::class, 'show'])->name('VentaPropuesta.show');
+        Route::get('/mostrarventas/{id}', [VentaPropuestaController::class, 'index_ventas'])->name('mostrarventas');
+    
+        Route::get('/errorVenta', function () {
+            return view('errorVenta');
+        })->name('errorVenta');
+    
+        Route::get('/compra/{id}', [VentaPropuestaController::class, 'show'])->name('compra.show');
+        Route::resource('venedors', VendedorController::class);
+        Route::resource('compras', CompraController::class);
+        
+        Route::get('/crearCompraProduct', [ProducteController::class, 'createCompraProducte'])->name('productes.createCompraProducte');
+        Route::post('productes/storeProducteCompra', [ProducteController::class, 'storeCompraProducte'])->name('productes.storeCompraProducte');
+        
+        Route::get('/grafico-productos', [ProducteController::class, 'graficoProductos'])->name('productes.graficoProductos');
     });
 });
-Route::get('/compra/{id}', [VentaPropuestaController::class, 'show'])->name('compra.show');
-Route::resource('venedors', VendedorController::class);
-Route::resource('compras', CompraController::class);
 
-Route::get('/crearCompraProduct', [ProducteController::class, 'createCompraProducte'])->name('productes.createCompraProducte');
-Route::post('productes/storeProducteCompra', [ProducteController::class, 'storeCompraProducte'])->name('productes.storeCompraProducte');
-
-Route::get('/grafico-productos', [ProducteController::class, 'graficoProductos'])->name('productes.graficoProductos');
 
 require __DIR__.'/auth.php';
